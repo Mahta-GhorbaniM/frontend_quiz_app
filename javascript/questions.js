14d32eafbabb8e229a114dc9a40b06734575ed94
Mahta Ghorbani <mahta_gh@hotmail.com>
2025-03-09 23:29:40 +0100

    completed quiz app

---
+++ javascript/questions.js
@@ -0,0 +1,165 @@
+
+
+const questions = [
+    {
+        category: "Geografi",
+        questions: [
+            {
+                question: "Vad heter världens längsta flod?",
+                options: ["Amazonas", "Nilen", "Yangtze", "Mississippi"],
+                correctAnswer: 1,
+            },
+            {
+                question: "Vilket land har flest invånare?",
+                options: ["USA", "Indien", "Kina", "Brasilien"],
+                correctAnswer: 2,
+            },
+            {
+                question: "Vilket är världens största land?",
+                options: ["Kanada", "USA", "Kina", "Ryssland"],
+                correctAnswer: 3,
+            },
+            {
+                question: "Vilken är världens största ö?",
+                options: ["Grönland", "Madagaskar", "Borneo", "Australien"],
+                correctAnswer: 0,
+            },
+            {
+                question: "Vilket land ligger på två kontinenter?",
+                options: ["Egypten", "Spanien", "Ryssland", "Turkiet"],
+                correctAnswer: 3,
+            },
+            {
+                question: "Vilken är världens största öken?",
+                options: ["Sahara", "Arktiska öknen", "Gobi", "Antarktis"],
+                correctAnswer: 3,
+            },
+            {
+                question: "Vilken stad är huvudstad i Australien?",
+                options: ["Sydney", "Melbourne", "Canberra", "Perth"],
+                correctAnswer: 2,
+            },
+            {
+                question: "Vilket är Sveriges största landskap?",
+                options: ["Dalarna", "Skåne", "Lappland", "Jämtland"],
+                correctAnswer: 2,
+            },
+            {
+                question: "Till vilket land tillhör Påskön?",
+                options: ["Chile", "Kanada", "Grekland", "Sverige"],
+                correctAnswer: 0,
+            },
+            {
+                question: "Vad heter Sveriges längsta flod?",
+                options: ["Luleälven", "Göta älv", "Indalsälven", "Ångermanälven"],
+                correctAnswer: 1,
+            },
+            {
+                question: "Vilken ö är Sveriges största?",
+                options: ["Öland", "Gotland", "Södertörn", "Väddö"],
+                correctAnswer: 1,
+            },
+            {
+                question: "Vilket land har den längsta kustlinjen?",
+                options: ["USA", "Chile", "Kanada", "Argentina"],
+                correctAnswer: 2,
+            },
+            {
+                question: "Vilket är det största landet i Sydamerika?",
+                options: ["Brasilien", "Chile", "Peru", "Argentina"],
+                correctAnswer: 0,
+            },
+            {
+                question: "Vilket land i Afrika är känt som Afrikas Pärla?",
+                options: ["Egypten", "Tanzania", "Sudan", "Uganda"],
+                correctAnswer: 3,
+            },
+            {
+                question: "Vad heter jordens största hav?",
+                options: ["Indiska oceanen", "Atlanten", "Stilla havet", "Antarktiska oceanen"],
+                correctAnswer: 2,
+            },
+        ]
+    },
+
+    {
+        category: "Matematik",
+        questions: [
+            {
+                question: "Det är -20 grader ute. Lite senare är det 3 grader kallare. Klicka på knappen med rätt uträkning och rätt svar!",
+                options: ["-20 + 3 = -23", "-20 - 3 = -23", "-20 + (-3) = -17", "-3 - (-20) = -22"],
+                correctAnswer: 1,
+            },
+            {
+                question: "Vad är -7 * 8?",
+                options: ["56", "-48", "-56", "-67"],
+                correctAnswer: 2,
+            },
+            {
+                question: "Vad är roten av 144?",
+                options: ["12", "14", "13", "15"],
+                correctAnswer: 0,
+            },
+            {
+                question: "En tröja kostar 80kr. Den sänks med 10%. Vad är nya priset?",
+                options: ["70kr", "72kr", "78kr", "75kr"],
+                correctAnswer: 1,
+            },
+            {
+                question: "Ett par byxor kostar 170kr. Priset höjs med 20%. Beräkna höjningen.",
+                options: ["30kr", "34kr", "20kr", "40kr"],
+                correctAnswer: 1,
+            },
+            {
+                question: "Skriv om bråket 1/4 i decimalform.",
+                options: ["0,4", "1,4", "0,1", "0,25"],
+                correctAnswer: 3,
+            },
+            {
+                question: "Klicka på det uttrycket som är en ekvation",
+                options: ["3x + 3 = 6", "x", "3 + x * 3 =", "6 - x"],
+                correctAnswer: 0,
+            },
+            {
+                question: "Vilket värde är lösningen till ekvationen x + 7 = 25?",
+                options: ["20", "7", "18", "25"],
+                correctAnswer: 2,
+            },
+            {
+                question: "Vilken vinkelsumma har en triangel?",
+                options: ["90 grader", "360 grader", "45 grader", "180 grader"],
+                correctAnswer: 3,
+            },
+            {
+                question: "En kvadrat har sidlängden 7 cm. Beräkna arean.",
+                options: ["49", "14", "28", "70"],
+                correctAnswer: 0,
+            },
+            {
+                question: "I en rektangel är ena sidan 9 cm och en annan sida 4 cm. Beräkna omkretsen.",
+                options: ["26", "36", "22", "17"],
+                correctAnswer: 0,
+            },
+            {
+                question: "Lös ekvationen 2x + 3 = 15",
+                options: ["15", "6", "3", "2"],
+                correctAnswer: 1,
+            },
+            {
+                question: "Klicka på den multiplikation som betyder samma sak som 4 + 4 + 4 + 4 + 4.",
+                options: ["4 * 4", "5 * 4", "6 * 4", "4 * 6"],
+                correctAnswer: 1,
+            },
+            {
+                question: "En triangel har sidlängderna 5 cm, 9 cm och 14 cm. Hur stor omkrets har denna triangel?",
+                options: ["14", "10", "18", "28"],
+                correctAnswer: 3,
+            },
+            {
+                question: "Avrunda 64 till närmaste tiotal.",
+                options: ["60", "40", "70", "100"],
+                correctAnswer: 0,
+            },
+        ]
+    },
+];
---
+++ javascript/script.js
@@ -0,0 +1,161 @@
+
+const answerOptions = document.querySelector(".answers");
+const nextQuestion = document.querySelector(".next-btn");
+const questionStatus = document.querySelector(".q-status");
+const timerDisplay = document.querySelector(".time");
+const resultCon = document.querySelector(".result-container");
+const quizCon = document.querySelector(".quiz-container");
+const appCon = document.querySelector(".app-container");
+const qTitle = document.querySelector(".q-title");
+
+
+let quizCategory = "geografi";
+let currentQuestion = null;
+const qIndexHistory = [];
+let numbQuestion = 5;
+const TIME_LIMIT = 10;
+let currentTime = TIME_LIMIT;
+let timer = null;
+let correctCount = 0;
+
+const showResult = () =>{
+    quizCon.style.display = "none";
+    resultCon.style.display = "block";
+
+    const resultText = `Du svarade <b>${correctCount}</b> av <b>${numbQuestion}</b>
+        frågor rätt,`;
+    document.querySelector(".result-msg").innerHTML = resultText;
+
+}
+
+const resetTimer = () =>{
+    clearInterval(timer);
+    currentTime = TIME_LIMIT
+    timerDisplay.textContent = `${currentTime}s`;
+
+}
+
+const startTimer = () => {
+    timer = setInterval(() => {
+        currentTime--;
+        timerDisplay.textContent = `${currentTime}s`;
+
+        if(currentTime <= 0){
+            clearInterval(timer);
+            highlightCorrectAnswer();
+            nextQuestion.style.visibility="visible";
+            quizCon.querySelector(".q-timer").style.background = "#c31402";
+
+            answerOptions.querySelectorAll(".a-option").forEach(option => option.style.pointerEvents="none");
+            
+        }
+    }, 1000);
+}
+
+const getRandomQuestion = () => {
+    const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions || [];
+
+    if (qIndexHistory.length >= Math.min(categoryQuestions.length, numbQuestion)) {
+        return showResult();
+    }
+
+    // Filtrera ut frågor som inte har valts ännu
+    const availableQuestions = categoryQuestions.filter((_, index) => !qIndexHistory.includes(index));
+
+    // Välj en slumpmässig fråga
+    const randomQuestion = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+
+    // Hitta rätt index för frågan i originalarrayen
+    const qIndex = categoryQuestions.findIndex(q => q.question === randomQuestion.question);
+    qIndexHistory.push(qIndex);
+
+    return randomQuestion;
+};
+
+
+const highlightCorrectAnswer = () => {
+    const correctOption = answerOptions.querySelectorAll(".a-option")[currentQuestion.correctAnswer];
+    correctOption.classList.add("correct");
+    const iconHTML = `<span class = "material-icons">check_circle</span>`;
+    correctOption.insertAdjacentHTML("beforeend", iconHTML);
+}
+
+const handleAnswer = (option,ansIndex) =>{
+    clearInterval(timer);
+    const isCorrect = currentQuestion.correctAnswer === ansIndex;
+    option.classList.add(isCorrect ? 'correct' : 'incorrect');
+
+    !isCorrect ? highlightCorrectAnswer() : correctCount++;
+
+    const iconHTML = `<span class = "material-icons">${isCorrect ? 'check_circle' : 'cancel'}</span>`;
+    option.insertAdjacentHTML("beforeend", iconHTML);
+
+    answerOptions.querySelectorAll(".a-option").forEach(option => option.style.pointerEvents="none");
+    nextQuestion.style.visibility="visible";
+} 
+
+const renderQuestion =() => {
+    currentQuestion = getRandomQuestion();
+    if(!currentQuestion) return;
+    
+    resetTimer();
+    startTimer();
+
+    answerOptions.innerHTML = "";
+    nextQuestion.style.visibility="hidden";
+    quizCon.querySelector(".q-timer").style.background = "#32313c";
+    document.querySelector(".q-question").textContent= currentQuestion.question;
+    questionStatus.innerHTML = `<b>${qIndexHistory.length}</b> of <b>${numbQuestion}</b> Questions`;
+    qTitle.innerHTML = `Quize App: ${quizCategory}`;
+    
+
+    currentQuestion.options.forEach((option ,index) => {
+        const li = document.createElement("li");
+        li.classList.add("a-option");
+        li.textContent = option;
+        answerOptions.appendChild(li);
+        li.addEventListener("click", () => handleAnswer(li,index));
+    });
+}
+
+const categoryMapping = {
+    "Geografi": "Geografi",
+    "Matematik": "Matematik"
+};
+
+const startQuiz = () => {
+    appCon.style.display = "none";
+    quizCon.style.display = "block";
+    qIndexHistory.length = 0;
+
+    // Hämta den aktiva knappens textinnehåll (t.ex. "Geografi")
+    const activeCategoryText = appCon.querySelector(".category-btn.active").textContent;
+
+    // Mappa den svenska texten på knappen till rätt kategori i questions.js
+    quizCategory = categoryMapping[activeCategoryText] || "Geografi";  // Standard till "Geografi" om inget matchas
+
+    numbQuestion = parseInt(appCon.querySelector(".no-q.active").textContent);
+
+    renderQuestion();
+}
+
+
+document.querySelectorAll(".category-btn, .no-q").forEach(option => {
+    option.addEventListener("click", () => {
+        option.parentNode.querySelector(".active").classList.remove("active");
+        option.classList.add("active");
+    })
+})
+
+const restQuiz = () =>{
+    resetTimer();
+    correctCount = 0;
+    qIndexHistory.length=0;
+    appCon.style.display = "block";
+    resultCon.style.display = "none";
+}
+
+renderQuestion();
+nextQuestion.addEventListener("click", renderQuestion);
+document.querySelector(".try-again").addEventListener("click", restQuiz);
+document.querySelector(".start-btn").addEventListener("click", startQuiz);
